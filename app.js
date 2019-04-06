f5431cafed38a4b7fb8a3a50ac57901e786d48f7
mahesh maharana <maharanamahesh6@gmail.com>
2019-04-06 19:14:29 +0530

    Ar

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+const port = process.env.PORT || 4000;
+
+
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+  
+app.use('/', router)
+
+app.listen(port, ()=>{
+    console.log(`Server started at ${port}`);
+})
